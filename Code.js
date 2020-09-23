b5261129b4e2c703dbe507265bc5c3b542eb0c9b
palicron <elgranpalicron@gmail.com>
2020-09-22 22:16:40 -0500

    toggle del shop y el menu

--- Code.js
+++ Code.js
@@ -1,5 +1,7 @@
 var menu = [];
-
+var Menu = "";
+var Shop = "";
+const order = [];
 req = new Promise((resolve, reject) => {
   ls = new XMLHttpRequest();
   ls.open(
@@ -25,6 +27,8 @@ req.then((res) => {
 });
 
 window.onload = function buildtable() {
+  Menu = document.getElementById("MenuCont");
+  Shop = document.getElementById("ShopContener");
   buildBurgers(0);
 };
 
@@ -44,9 +48,32 @@ buildBurgers = function (index) {
     Newcard += ' <h5 class="card-title">' + Burgers[i].name + "</h5>";
     Newcard += '<p class="card-text">' + Burgers[i].description + "</p>";
     Newcard += '<p class="card-text">' + "$" + Burgers[i].price + "</p>";
-    Newcard += ' <a href="#" class="btn btn-primary bg-dark">Add to car</a>';
-
+    Newcard +=
+      ' <a id="Add" type="submit" class="btn btn-primary bg-dark">Add to car</a>';
     Newcard += " </div> </div> </div> ";
+
     $("#Card-Cont").append(Newcard);
+    document.getElementById("Add").addEventListener("click", addPer);
   }
+  Menu.hidden = false;
+  Shop.hidden = true;
 };
+
+const addPer = (ev) => {
+  ev.preventDefault();
+  console.log("asds");
+};
+const cancel = (ev) => {
+  ev.preventDefault();
+  console.log("asds");
+  order.length = 0;
+};
+
+const toggleshop = () => {
+  Menu.hidden = true;
+  Shop.hidden = false;
+};
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("confirm").addEventListener("click", addPer);
+  document.getElementById("cancel").addEventListener("click", cancel);
+});
