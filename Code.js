cc8a618b09887e0d9ae9da3249a1fe475068d6ae
palicron <elgranpalicron@gmail.com>
2020-09-22 23:34:14 -0500

    Se termino toda la funcinalidad pedida falta el css

--- Code.js
+++ Code.js
@@ -1,6 +1,8 @@
 var menu = [];
 var Menu = "";
 var Shop = "";
+var CardContainer = "";
+var numItems = 0;
 const order = [];
 req = new Promise((resolve, reject) => {
   ls = new XMLHttpRequest();
@@ -29,12 +31,40 @@ req.then((res) => {
 window.onload = function buildtable() {
   Menu = document.getElementById("MenuCont");
   Shop = document.getElementById("ShopContener");
+  CardContainer = document.getElementById("Card-Cont");
   buildBurgers(0);
+  CardContainer.addEventListener("click", (event) => {
+    const isButton = event.target.nodeName === "A";
+    if (!isButton) {
+      return;
+    }
+    var dd = event.target.parentElement;
+    var romp = false;
+    numItems++;
+    for (let i = 0; i < order.length; i++) {
+      if (order[i].description == dd.children[0].innerHTML) {
+        romp = true;
+        order[i].quantity += 1;
+        break;
+      }
+    }
+    if (!romp) {
+      var item = {
+        item: order.length + 1,
+        quantity: 1,
+        description: dd.children[0].innerHTML,
+        unitPrice: dd.children[2].innerHTML,
+      };
+      order.push(item);
+    }
+    popularTabla();
+  });
 };
 
 buildBurgers = function (index) {
   var Burgers = menu[index].products;
-  var CardContainer = document.getElementById("Card-Cont");
+  document.getElementById("TipoTitulo").innerHTML = menu[index].name;
+
   CardContainer.innerHTML = "";
   for (let i = 0; i < Burgers.length; i++) {
     var Newcard = "";
@@ -45,15 +75,14 @@ buildBurgers = function (index) {
       Burgers[i].image +
       '" alt="Card image cap">';
     Newcard += ' <div class="card-body">';
-    Newcard += ' <h5 class="card-title">' + Burgers[i].name + "</h5>";
+    Newcard += ' <h5 id="name"class="card-title">' + Burgers[i].name + "</h5>";
     Newcard += '<p class="card-text">' + Burgers[i].description + "</p>";
-    Newcard += '<p class="card-text">' + "$" + Burgers[i].price + "</p>";
+    Newcard += '<p class="card-text"> ' + "$ " + Burgers[i].price + "</p>";
     Newcard +=
       ' <a id="Add" type="submit" class="btn btn-primary bg-dark">Add to car</a>';
     Newcard += " </div> </div> </div> ";
 
     $("#Card-Cont").append(Newcard);
-    document.getElementById("Add").addEventListener("click", addPer);
   }
   Menu.hidden = false;
   Shop.hidden = true;
@@ -61,17 +90,51 @@ buildBurgers = function (index) {
 
 const addPer = (ev) => {
   ev.preventDefault();
-  console.log("asds");
+  console.log(order);
 };
 const cancel = (ev) => {
   ev.preventDefault();
-  console.log("asds");
+
   order.length = 0;
+  numItems = 0;
+  popularTabla();
 };
 
 const toggleshop = () => {
   Menu.hidden = true;
   Shop.hidden = false;
+};
+
+const popularTabla = () => {
+  var table = document.getElementById("myTable");
+  var total = document.getElementById("Total");
+  var list = document.getElementById("cartList");
+  table.innerHTML = "";
+  total.innerHTML = "";
+  var sum = 0;
+  for (let i = 0; i < order.length; i++) {
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+
+    cell1.innerHTML = order[i].item;
+    cell2.innerHTML = order[i].quantity;
+    cell3.innerHTML = order[i].description;
+    cell4.innerHTML = order[i].unitPrice;
+    var suma = order[i].unitPrice;
+
+    cell5.innerHTML =
+      parseInt(order[i].unitPrice.replace("$", "")) *
+      parseInt(order[i].quantity);
+    sum +=
+      parseInt(order[i].unitPrice.replace("$", "")) *
+      parseInt(order[i].quantity);
+  }
+  total.innerHTML = "ToTal: " + sum;
+  list.innerHTML = numItems + " Items";
 };
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("confirm").addEventListener("click", addPer);
